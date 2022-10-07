c15d967c8417706e0a7da8776d1f7a9b54813872
James Nguyen <103847844+tigermilktpapi@users.noreply.github.com>
2022-10-06 18:38:55 -0700

    Completing first python activity files

---
+++ Getting-Started-with-Python-JamesN11/about_me.py
@@ -0,0 +1,25 @@
+#1 & #2
+myname = "James Nguyen"
+prefname = "James Nguyen"
+print(myname)
+#3
+age = 20
+print(age)
+#4
+yes = True
+print("Is it true that James has used Python before?", yes)
+
+#5
+favhobbies = [
+    "Swimming", "Eating", "Watching One Piece (as of recently xd)", "Reading"
+]
+print("My favorite hobbies are:", favhobbies)
+#6
+favthings_dict = {
+    1: "Books",
+    2: "Training for Triathlon",
+    3: "Oolong Tea w/ Boba"
+}
+print("My favorite things/activites are:", favthings_dict)
+
+all_vars = dict(vars())
